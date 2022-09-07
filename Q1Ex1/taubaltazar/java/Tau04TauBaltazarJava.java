98fe1aa3c14ff0cc99743c6298b0d135f8c56be2
mythicalmilk <96482130+mythicalmilk@users.noreply.github.com>
2022-09-07 08:55:20 +0800

    Add files via upload

---
+++ Q1Ex1/taubaltazar/java/Tau04TauBaltazarJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tau04.taubaltazar.java;
+
+/**
+ *
+ * @author TAU
+ */
+public class Tau04TauBaltazarJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name1 = "John";
+        int ageJohn = 26;
+        int heightJohn = 180;
+        
+        String name2 = "James";
+        int ageJames = 34;
+        int heightJames = 165;
+        
+        String name3 = "Jerry";
+        int ageJerry = 45;
+        int heightJerry = 170;
+        
+        System.out.print("Person 1");
+    }
+    
+}
