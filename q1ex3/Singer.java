21c985f6283b3b144b05c0f2bb5ec964d0a11529
mythicalmilk <96482130+mythicalmilk@users.noreply.github.com>
2022-09-23 12:11:46 +0800

    Add files via upload

---
+++ q1ex3/Singer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex3;
+
+/**
+ *
+ * @author TAU
+ */
+public class Singer {
+    String name;
+    int noOfPerformances;
+    double earnings;
+    Song favoriteSong;
+    
+}
